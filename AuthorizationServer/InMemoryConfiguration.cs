9eeca43eef1922d550ffb95b8710a5b343b1c903
xiongz <xiongz@LL-04-D041>
2018-06-26 14:16:39 +0800

    add AuthorizationServer sample project

---
+++ AuthorizationServer/InMemoryConfiguration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthorizationServer
+{
+    public class InMemoryConfiguration
+    {
+        public static IConfiguration Configuration { get; set; }
+
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("clientservice", "CAS Client Service"),
+                new ApiResource("productservice", "CAS Product Service"), 
+                new ApiResource("agentservice", "CAS Agent Service"), 
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client.api.service",
+                    ClientSecrets = new[]
+                    {
+                        new Secret("clientsecert".Sha256()),
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] {"clientservice"}
+                },
+                new Client()
+                {
+                    ClientId = "product.api.service",
+                    ClientSecrets = new []
+                    {
+                        new Secret("productsecret".Sha256()), 
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "clientservice", "productservice" }
+                }, 
+                new Client()
+                {
+                    ClientId = "agent.api.service",
+                    ClientSecrets = new []
+                    {
+                        new Secret("agentsecert".Sha256()), 
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] {"agentservice", "clientservice", "productservice"}
+                }, 
+            };
+        }
+
+
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "10001",
+                    Username = "edison@hotmail.com",
+                    Password = "edisonpassword"
+                },
+                new TestUser
+                {
+                    SubjectId = "10002",
+                    Username = "andy@hotmail.com",
+                    Password = "andypassword"
+                },
+                new TestUser
+                {
+                    SubjectId = "10003",
+                    Username = "leo@hotmail.com",
+                    Password = "leopassword"
+                }
+            };
+        }
+    }
+}
---
+++ AuthorizationServer/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            InMemoryConfiguration.Configuration = this._configuration;
+            services.AddIdentityServer()
+                    //.AddDeveloperSigningCredential()
+                    .AddSigningCredential(new X509Certificate2(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _configuration["Certificates:CerPath"]), _configuration["Certificates:Password"]))
+                    .AddTestUsers(InMemoryConfiguration.GetUsers().ToList())
+                    .AddInMemoryClients(InMemoryConfiguration.GetClients())
+                    .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
