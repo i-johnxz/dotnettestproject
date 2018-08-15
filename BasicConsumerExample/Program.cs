6f385308840238ae87e3788682cbeac3e5db193d
xiongz <xiongz@LL-04-D041>
2018-08-15 16:14:44 +0800

    add kafka demo

---
+++ BasicConsumerExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace BasicConsumerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new Dictionary<string, object>
+            {
+                {"group.id", "test-consumer-group"},
+                {"bootstrap.servers", "10.100.7.46:9092"},
+                {"auto.commit.interval.ms", 5000},
+                {"auto.offset.reset", "earliest"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnMessage += (sender, message) =>
+                {
+                    Console.WriteLine($"Read '{message.Value}' from: {message.TopicPartitionOffset}");
+                };
+
+                consumer.OnError += (sender, error) => { Console.WriteLine($"Error: {error}"); };
+
+                consumer.OnConsumeError += (sender, message) =>
+                {
+                    Console.WriteLine($"Consume error ({message.TopicPartitionOffset}): {message.Error}");
+                };
+
+                consumer.Subscribe("my-topic");
+
+                while (true)
+                {
+                    consumer.Poll(TimeSpan.FromMilliseconds(100));
+                }
+
+            }
+        }
+    }
+}
---
+++ BasicProduerExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace BasicProduerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                {"bootstrap.servers", "10.100.7.46:9092"}
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                
+
+                while (true)
+                {
+                    Console.WriteLine("请输入字符串:");
+                    var text = Console.ReadLine();
+                    var dr = producer.ProduceAsync("my-topic", null, text).Result;
+                    Console.WriteLine($"Delivered '{dr.Value}' to : {dr.TopicPartitionOffset}");
+                }
+            }
+        }
+    }
+}
