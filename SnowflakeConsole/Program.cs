988244950d94711ccfa45f429a2d3ca3e11c469f
xiongz <xiongz@LL-04-D043>
2019-01-02 10:55:31 +0800

    add Snowflake

---
+++ SnowflakeConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Snowflake.Core;
+
+namespace SnowflakeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new IdWorker(1, 1, 1);
+            long id = worker.NextId();
+            Console.WriteLine(id);
+            Console.Read();
+        }
+    }
+}
