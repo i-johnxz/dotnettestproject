be8e451361c62f5a6d14874ca2ed602a92cac310
xiongz <xiongz@LL-04-D043>
2019-01-04 10:57:47 +0800

    add MediaRWebDemo

---
+++ MediaRWebDemo/Commands/Ping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MediaRWebDemo.Commands
+{
+    public class Ping: IRequest<string>
+    {
+    }
+}
---
+++ MediaRWebDemo/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaRWebDemo.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaRWebDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ValuesController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var result = await _mediator.Send(new Ping());
+            return result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
