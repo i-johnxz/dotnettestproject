f6ecbaff2f566a1bdcd2d0da0e8861fae397ed08
xiongz <a824440331@gmail.com>
2019-02-02 09:57:16 +0800

    add CerasConsoleApp project

---
+++ CerasConsoleApp/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CerasConsoleApp
+{
+    public class Person
+    {
+        public string Name;
+        public int Health;
+        public Person BestFriend;
+
+    }
+}
---
+++ CerasConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ceras;
+
+namespace CerasConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // 1.) Simple usage
+            // aka. "I'm here for the cool features! I want to optimize for max-performance later"
+            var person = new Person { Name = "riki", Health = 100 };
+
+            var serializer = new CerasSerializer();
+            
+
+            var data = serializer.Serialize(person);
+            data.VisualizePrint("Simple Person");
+
+            var clone1 = serializer.Deserialize<Person>(data);
+            Console.WriteLine($"Clone: Name={clone1.Name}, Health={clone1.Health}");
+            Console.Read();
+        }
+    }
+
+
+
+}
