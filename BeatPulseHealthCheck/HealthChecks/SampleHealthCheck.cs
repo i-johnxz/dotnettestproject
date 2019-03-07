427b2dd9ad7208d016986cd1603645cd79e3f4e2
xiongz <a824440331@gmail.com>
2019-03-07 11:10:45 +0800

    add Health Check

---
+++ BeatPulseHealthCheck/HealthChecks/SampleHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics.Health;
+
+namespace BeatPulseHealthCheck.HealthChecks
+{
+    public class SampleHealthCheck : HealthCheck
+    {
+        public SampleHealthCheck()
+            : base("Sample Health Check") { }
+
+        protected override ValueTask<HealthCheckResult> CheckAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (DateTime.UtcNow.Second <= 20)
+            {
+                return new ValueTask<HealthCheckResult>(HealthCheckResult.Degraded());
+            }
+
+            if (DateTime.UtcNow.Second >= 40)
+            {
+                return new ValueTask<HealthCheckResult>(HealthCheckResult.Unhealthy());
+            }
+
+            return new ValueTask<HealthCheckResult>(HealthCheckResult.Healthy());
+        }
+    }
+}
---
+++ BeatPulseHealthCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.AspNetCore.Health;
+using App.Metrics.Health;
+using App.Metrics.Health.Checks.Sql;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BeatPulseHealthCheck
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureHealthWithDefaults(builder =>
+                {
+                    const long threshold = 8589934592;
+                    const long VirtualMemorySize = 2418925581107;
+                    //builder.HealthChecks.AddCheck("DatabaseConnected", () => new ValueTask<HealthCheckResult>(HealthCheckResult.Healthy("Database Connection OK")));
+                    builder.HealthChecks.AddSqlCheck("Asset", @"Server=(localdb)\projects;Database=Asset;Integrated Security=true;", TimeSpan.FromSeconds(10));
+                    builder.HealthChecks.AddProcessPrivateMemorySizeCheck("Private Memory Size", threshold);
+                    builder.HealthChecks.AddProcessVirtualMemorySizeCheck("Virtual Memory Size", VirtualMemorySize);
+                    builder.HealthChecks.AddProcessPhysicalMemoryCheck("Working Set", threshold);
+                })
+                .UseHealth()
+                .UseStartup<Startup>();
+    }
+}
