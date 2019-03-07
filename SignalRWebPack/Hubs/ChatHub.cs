b6e1a45ad07e5e5955ba56852011510aec18b891
xiongz <a824440331@gmail.com>
2019-03-07 14:40:32 +0800

    add signlar demo

---
+++ SignalRWebPack/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebPack.Hubs
+{
+    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task NewMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
