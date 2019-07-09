8187828d93e707541012e1231e2a98b4cddc394c
xiongz <a824440331@gmail.com>
2019-07-09 10:47:29 +0800

    add project

---
+++ NewRedisCaching/Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NewRedisCaching
+{
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
