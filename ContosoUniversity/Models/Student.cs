88bee5ac2a9968d117d692b7da3ec8ea272b0ec9
xiongz <a824440331@gmail.com>
2019-07-09 17:54:33 +0800

    add project

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName  { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName => LastName + ", " + FirstMidName;
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
